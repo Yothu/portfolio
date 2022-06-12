64587353b0bbe617ff1e81a031d5ab90caf6eaad
David Vergaray <david.vergaray.a@gmail.com>
2022-06-12 18:18:05 -0400

    Created Main page and added styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Route, Routes } from 'react-router';
+import styled from 'styled-components';
+import Main from './pages/Main/Main';
+
+const WholePage = styled.div``;
+
 const App = () => (
-  <div className="App">
-    a
-  </div>
+  <WholePage>
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
+  </WholePage>
 );
 
 export default App;
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,7 @@
+const Main = () => (
+  <div>
+    <h1>Main</h1>
+  </div>
+);
+
+export default Main;
