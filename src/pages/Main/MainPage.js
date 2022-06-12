ef76d69dbfc17d594b0edfbaf2c402af8307d0a5
David Vergaray <david.vergaray.a@gmail.com>
2022-06-12 19:47:31 -0400

    Finish front section main page basic design

--- src/pages/Main/MainPage.js
+++ src/pages/Main/MainPage.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
+import { BsGithub, BsLinkedin, BsFolderFill } from 'react-icons/bs';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { IconContext } from 'react-icons';
 
 const Navbar = styled.nav`
   display: flex;
-  padding: 0.2rem;
+  padding: 0rem 1rem;
   align-items: center;
   background-color: #3e3c3c;
   justify-content: space-between;
@@ -12,7 +13,6 @@ const Navbar = styled.nav`
 
 const NavTitle = styled.span`
   color: #fff;
-  margin: 0rem 0.2rem;
 `;
 
 const Main = styled.main`
@@ -28,15 +28,37 @@ const Name = styled.span`
   color: #f55800;
 `;
 
+const Job = styled.span`
+  color: #fff;
+`;
+
+const Description = styled.p`
+  color: #fff;
+`;
+
 const FrontSection = styled.section`
-  background-color: #000;
-  height: 100vh;
+  display: flex;
+  min-height: 100vh;
+  align-items: center;
+  flex-direction: column;
+  background-color: #1c1a19;
+  padding: 0 1rem;
+`;
+
+const FrontContainer = styled.div`
+  margin: 0.5rem;
+  margin: 3rem 0;
+`;
+
+const SocialMediaContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
 `;
 
 const MainPage = () => {
   const IconStyle = {
     color: 'white',
-    margin: '0.3rem',
+    margin: '0.3rem 0rem',
   };
 
   // const [isOpen, setIsOpen] = useState(false);
@@ -56,10 +78,32 @@ const MainPage = () => {
       </header>
       <Main>
         <FrontSection>
-          <div>
-            <Title>Welcome!</Title>
-            <Name>I am David</Name>
-          </div>
+          <FrontContainer>
+            <div>
+              <Title>Welcome!</Title>
+              <Name>I am David</Name>
+            </div>
+            <div>
+              <Job>I am a Web Developer</Job>
+              <Description>
+                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              </Description>
+            </div>
+            <SocialMediaContainer>
+              <IconContext.Provider value={{ size: 30, style: IconStyle }}>
+                <BsGithub />
+                <BsLinkedin />
+                <BsFolderFill />
+              </IconContext.Provider>
+            </SocialMediaContainer>
+          </FrontContainer>
         </FrontSection>
       </Main>
     </>
