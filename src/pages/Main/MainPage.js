51c6607f93e1f5b4fd4de13af40dc8989996488c
David Vergaray <david.vergaray.a@gmail.com>
2022-06-22 19:06:11 -0400

    Change to grid

--- src/pages/Main/MainPage.js
+++ src/pages/Main/MainPage.js
@@ -10,15 +10,21 @@ const Main = styled.main`
   background-color: black;
 `;
 
+// const FrontSection = styled.section`
+//   background-image: linear-gradient(to bottom, #020B16, #072141);
+//   justify-content: center;
+//   flex-direction: column;
+//   align-items: center;
+//   min-height: 100vh;
+//   padding: 0 1rem;
+//   display: flex;
+// `;
+
 const FrontSection = styled.section`
-  border-radius: 0 0 0 40px;
   background-image: linear-gradient(to bottom, #020B16, #072141);
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
+  grid-template-columns: 50% 50%;
   min-height: 100vh;
-  padding: 0 1rem;
-  display: flex;
+  display: grid;
 `;
 
 const FrontContainer = styled.div`
