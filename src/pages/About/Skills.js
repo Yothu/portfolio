e7fe9870dac0da6cbe84a456b4ab7c5ba2c36e09
David Vergaray <david.vergaray.a@gmail.com>
2022-07-13 17:07:20 -0400

    Add Skills component to About section

---
+++ src/pages/About/Skills.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  flex-basis: 25rem;
+  flex-grow: 3;
+`;
+
+const Skills = () => (
+  <Container>
+    asd
+  </Container>
+);
+
+export default Skills;
